6f2e150b5c36fd9bd227a2d563c824aa42031a3e
Dmytro Baida <dmytrobaida@outlook.com>
2025-04-27 10:18:40 +0000

    add ssl

--- apps/file-index/src/main.ts
+++ apps/file-index/src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { readFile } from 'fs/promises';
 import { join } from 'path';
 import { AppModule } from 'src/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    httpsOptions: {
+      key: await readFile('./cert/key.pem'),
+      cert: await readFile('./cert/cert.pem'),
+    },
+  });
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
