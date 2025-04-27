26bbfb3d154f9e5cc4c8df87bf4b287027fec9c9
Dmytro Baida <dmytrobaida@outlook.com>
2025-04-27 12:03:16 +0300

    enable app cors

--- apps/file-index/src/main.ts
+++ apps/file-index/src/main.ts
@@ -4,7 +4,9 @@ import { join } from 'path';
 import { AppModule } from 'src/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
