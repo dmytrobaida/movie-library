b0a7c92e78ef6c19a86a9bcc076331ef82e7e087
Dmytro Baida <dmytrobaida@outlook.com>
2025-04-27 12:10:38 +0300

    another approach to enable cors

--- apps/file-index/src/main.ts
+++ apps/file-index/src/main.ts
@@ -4,13 +4,17 @@ import { join } from 'path';
 import { AppModule } from 'src/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    cors: true,
-  });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
+
+  app.enableCors({
+    origin: '*',
+    methods: '*',
+    allowedHeaders: '*',
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
