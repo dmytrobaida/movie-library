77f271bb06a0621c2e276a338371c29f8bfdb891
Dmytro Baida <dmytrobaida@outlook.com>
2025-04-27 15:02:28 +0300

    remove cert

--- apps/file-index/src/main.ts
+++ apps/file-index/src/main.ts
@@ -1,16 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { readFile } from 'fs/promises';
 import { join } from 'path';
 import { AppModule } from 'src/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    httpsOptions: {
-      key: await readFile('./cert/key.pem'),
-      cert: await readFile('./cert/cert.pem'),
-    },
-  });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
